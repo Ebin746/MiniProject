faf3f31b6b95837dc5130a97f0dbc06fdfa8aa48
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2026-02-19 15:00:23 +0530

    fix:index

---
+++ src/mastra/tools/index.ts
@@ -0,0 +1,6 @@
+export * from './calculateFOIR';
+export * from './generateLoanPDF';
+export * from './getAvailableLoans';
+export * from './updateProfile';
+export * from './verifyKYC';
+export * from './getCreditScore';
