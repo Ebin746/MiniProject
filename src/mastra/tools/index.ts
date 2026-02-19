9c975b5162b0b93fa37d4d80027a651716adc75f
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2026-02-19 14:57:00 +0530

    chore: remove loan confirmation PDFs and related exports

--- src/mastra/tools/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './calculateFOIR';
-export * from './generateLoanPDF';
-export * from './getAvailableLoans';
-export * from './updateProfile';
-export * from './verifyKYC';
-export * from './getCreditScore';
