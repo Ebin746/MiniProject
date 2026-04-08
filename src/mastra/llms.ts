52bfb053689578fd73f4caa53af576a6fca4af3b
Ebin746 <146419456+Ebin746@users.noreply.github.com>
2026-04-09 00:17:36 +0530

    feat: update PRIMARY_MODEL and remove unused user schema fields

--- src/mastra/llms.ts
+++ src/mastra/llms.ts
@@ -2,13 +2,13 @@
 //export const PRIMARY_MODEL = 'groq/llama-3.3-70b-versatile';
 
 
-//export const PRIMARY_MODEL = 'cerebras/qwen-3-235b-a22b-instruct-2507';
+export const PRIMARY_MODEL = 'cerebras/qwen-3-235b-a22b-instruct-2507';
 
 
 
 //export const PRIMARY_MODEL = 'groq/openai/gpt-oss-120b';
 
-export const PRIMARY_MODEL = 'groq/moonshotai/kimi-k2-instruct';
+//export const PRIMARY_MODEL = 'groq/moonshotai/kimi-k2-instruct';
 //export const PRIMARY_MODEL = 'groq/moonshotai/kimi-k2-instruct-0905';
 
 //export const PRIMARY_MODEL='nvidia/nvidia/nemotron-3-super-120b-a12b';
